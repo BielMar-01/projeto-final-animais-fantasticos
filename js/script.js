9927809fbe21cb2336db48c2bed6402682a18f6e
Gabriel Marques <bielmarques922@gmail.com>
2022-03-31 20:22:37 -0300

    Projeto Final - Refatorando a Tooltip

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initAnimacaoScroll from './modules/animacao-scroll.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tab-nav.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -22,8 +22,10 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
+const tootip = new Tooltip('[data-tooltip');
+tootip.init();
+
 initAnimacaoScroll();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
